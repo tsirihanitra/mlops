4e70ac9eaae5cceae6a2d3d4a324c29a0c5af17f
tsirihanitra <150815263+tsirihanitra@users.noreply.github.com>
2026-03-30 14:55:59 +0300

    Delete mlops directory

--- mlops/tests/test_predict.py
+++
@@ -1,9 +0,0 @@
-from src.predict import preprocess
-
-def test_preprocess():
-    result = preprocess("Bonjour le Monde")
-    assert result == "bonjour le monde"
-    print("Test OK")
-
-if __name__ == '__main__':
-    test_preprocess()
